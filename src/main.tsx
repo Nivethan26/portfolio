021a9b9c250a9745582dd6ac3dd73388c360d618
Nivethan26 <nivethan.nive2000@gmail.com>
2026-04-01 21:45:22 +0530

    fix vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 
 // Set dark mode as default on first load
 if (typeof document !== "undefined") {
-	document.documentElement.classList.add("dark");
+  document.documentElement.classList.add("dark");
 }
 
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
